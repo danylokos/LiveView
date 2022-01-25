ed2d64c2bcc11fb0cd6ee6c99ed5fc96ae71066e
Danylo Kostyshyn <danylo.kostyshyn@gmail.com>
2022-01-25 11:44:06 -0800

    Initial Commit

---
+++ LiveView/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LiveView
+//
+//  Created by Danylo Kostyshyn on 25.01.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
